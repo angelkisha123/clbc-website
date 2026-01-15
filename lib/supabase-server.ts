34dcac391327a153b4cf0899c8f722663730af91
Gelaii123 <angelkishaarcenal10@gmail.com>
2026-01-15 11:49:42 +0800

    solve production

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -1,11 +1,17 @@
 import { createClient } from "@supabase/supabase-js";
+import { cookies } from "next/headers";
 
 export const supabaseServer = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   {
-    auth: {
-      persistSession: false,
+    global: {
+      fetch: async (url, options = {}) => {
+        return fetch(url, {
+          ...options,
+          cache: "no-store", // 🔥 THIS FIXES PROD
+        });
+      },
     },
   }
 );
